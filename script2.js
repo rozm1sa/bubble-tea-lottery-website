ae311309e64b54d9d94c839b34eb9715ce3fbdb3
rozm1sa <d.rozmisa@gmail.com>
2024-06-05 03:39:17 +0300

    big update

--- script2.js
+++ script2.js
@@ -1,3 +1,30 @@
+/*=============== SWIPER JS ===============*/
+let swiperCards = new Swiper(".card__content", {
+    loop: true,
+    spaceBetween: 32,
+    grabCursor: true,
+  
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  
+    breakpoints:{
+      600: {
+        slidesPerView: 2,
+      },
+      968: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
 const imgs = document.querySelectorAll('.img-select a');
 const imgBtns = [...imgs];
 let imgId = 1;
@@ -82,3 +109,4 @@ document.getElementById('lotteryForm').addEventListener('submit', function(e) {
 
 
 
+  
